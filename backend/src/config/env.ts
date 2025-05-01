b7eb75c3479ef3748d5daa8064fec1c625da678e
Mubtasim <mubtasimf443@gmail.com>
2025-05-01 20:58:04 +0600

    cloudinary api keys added

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,5 +6,11 @@ dotenv.config();
 
 type ENV_VALUE=string | undefined
 
+// SERVER
 export const PORT : ENV_VALUE =process.env.PORT ;
+// Database
 export const MONGO_DB_URL:ENV_VALUE=process.env.MONGO_DB_URL ;
+// FILE STORAGE
+export const ClOUDINARY_CLOUD_NAME:ENV_VALUE=process.env.ClOUDINARY_CLOUD_NAME ;
+export const ClOUDINARY_API_KEY:ENV_VALUE=process.env.ClOUDINARY_API_KEY ;
+export const ClOUDINARY_API_SECRET:ENV_VALUE=process.env.ClOUDINARY_API_SECRET ;
