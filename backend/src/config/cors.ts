a3d973c07209c7146e91b56e2ceef37506df01d4
Mubtasim <mubtasimf443@gmail.com>
2025-05-02 08:51:10 +0600

    cors and multer added

---
+++ backend/src/config/cors.ts
@@ -0,0 +1,16 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  ﷺ InshaAllah
+*/
+import express from 'express'
+
+export function cors(req :express.Request, res :express.Response, next :express.NextFunction) :void {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+         res.sendStatus(200);
+         return;
+    }
+    next();
+}
