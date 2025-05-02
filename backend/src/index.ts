56cd30e4e84d07ee1239b5573f592f3dbfd2a841
Mubtasim <mubtasimf443@gmail.com>
2025-05-02 11:57:04 +0600

    routers added to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,31 @@
 import express, { Express, Request, Response, Application } from 'express';
 import { PORT } from './config/env';
 import { connectDB } from './config/connectDB';
+import { cors } from './config/cors';
+import authRouter from './routes/auth';
+import searchRouter from './routes/search';
+import assetsRouter from './routes/asset';
+import userRouter from './routes/user';
 
 const port : number = Number(PORT ?? 4000) 
 async function main() {
 
+
+    // Variables
     const app: Application = express();
 
+
+    // Environmemt
     await connectDB();
-    
+
+
+    // routes
+    app.use('/api/auth' , assetsRouter);
+    app.use('/api/search' , searchRouter);
+    app.use('/api/assets' , assetsRouter);
+    app.use('/api/auth' , userRouter);
+
+
     app.get('/', (req: Request, res: Response) => {
         res.send('Welcome to Express & TypeScript Server');
     });
