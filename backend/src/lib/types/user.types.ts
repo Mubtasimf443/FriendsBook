4164ea5becda8bd037eb936e95cfd4297fe7bbd7
Mubtasim <mubtasimf443@gmail.com>
2025-05-02 10:42:36 +0600

    users model updated as needed

---
+++ backend/src/lib/types/user.types.ts
@@ -0,0 +1,276 @@
+/* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
+
+import { Document } from 'mongoose';
+
+export enum ProfileCreatedBy {
+    SELF = 'self',
+    PARENT = 'parent',
+    SIBLINGS = 'siblings',
+    RELATIVE = 'relative',
+    FRIEND = 'friend'
+}
+
+interface IUserImage {
+    url : String ,
+    id : String 
+}
+
+
+export enum Gender {
+    MALE = 'male',
+    FEMALE = 'female'
+}
+
+export enum Height {
+    // 4 feet range
+    FOOT_4_0 = "4 foot 0 inch",
+    FOOT_4_1 = "4 foot 1 inch",
+    FOOT_4_2 = "4 foot 2 inch",
+    FOOT_4_3 = "4 foot 3 inch",
+    FOOT_4_4 = "4 foot 4 inch",
+    FOOT_4_5 = "4 foot 5 inch",
+    FOOT_4_6 = "4 foot 6 inch",
+    FOOT_4_7 = "4 foot 7 inch",
+    FOOT_4_8 = "4 foot 8 inch",
+    FOOT_4_9 = "4 foot 9 inch",
+    FOOT_4_10 = "4 foot 10 inch",
+    FOOT_4_11 = "4 foot 11 inch",
+
+    // 5 feet range
+    FOOT_5_0 = "5 foot 0 inch",
+    FOOT_5_1 = "5 foot 1 inch",
+    FOOT_5_2 = "5 foot 2 inch",
+    FOOT_5_3 = "5 foot 3 inch",
+    FOOT_5_4 = "5 foot 4 inch",
+    FOOT_5_5 = "5 foot 5 inch",
+    FOOT_5_6 = "5 foot 6 inch",
+    FOOT_5_7 = "5 foot 7 inch",
+    FOOT_5_8 = "5 foot 8 inch",
+    FOOT_5_9 = "5 foot 9 inch",
+    FOOT_5_10 = "5 foot 10 inch",
+    FOOT_5_11 = "5 foot 11 inch",
+
+    // 6 feet range
+    FOOT_6_0 = "6 foot 0 inch",
+    FOOT_6_1 = "6 foot 1 inch",
+    FOOT_6_2 = "6 foot 2 inch",
+    FOOT_6_3 = "6 foot 3 inch",
+    FOOT_6_4 = "6 foot 4 inch",
+    FOOT_6_5 = "6 foot 5 inch",
+    FOOT_6_6 = "6 foot 6 inch",
+    FOOT_6_7 = "6 foot 7 inch",
+    FOOT_6_8 = "6 foot 8 inch",
+    FOOT_6_9 = "6 foot 9 inch",
+    FOOT_6_10 = "6 foot 10 inch",
+    FOOT_6_11 = "6 foot 11 inch",
+
+    // 7 feet range
+    FOOT_7_0 = "7 foot 0 inch",
+    FOOT_7_1 = "7 foot 1 inch",
+    FOOT_7_2 = "7 foot 2 inch",
+    FOOT_7_3 = "7 foot 3 inch",
+    FOOT_7_4 = "7 foot 4 inch",
+    FOOT_7_5 = "7 foot 5 inch",
+    FOOT_7_6 = "7 foot 6 inch",
+    FOOT_7_7 = "7 foot 7 inch",
+    FOOT_7_8 = "7 foot 8 inch",
+    FOOT_7_9 = "7 foot 9 inch",
+    FOOT_7_10 = "7 foot 10 inch",
+    FOOT_7_11 = "7 foot 11 inch",
+
+    // 8 feet range
+    FOOT_8_0 = "8 foot 0 inch",
+    FOOT_8_1 = "8 foot 1 inch",
+    FOOT_8_2 = "8 foot 2 inch",
+    FOOT_8_3 = "8 foot 3 inch",
+    FOOT_8_4 = "8 foot 4 inch",
+    FOOT_8_5 = "8 foot 5 inch",
+    FOOT_8_6 = "8 foot 6 inch",
+    FOOT_8_7 = "8 foot 7 inch",
+    FOOT_8_8 = "8 foot 8 inch",
+    FOOT_8_9 = "8 foot 9 inch",
+    FOOT_8_10 = "8 foot 10 inch",
+    FOOT_8_11 = "8 foot 11 inch",
+
+    // 9 feet range
+    FOOT_9_0 = "9 foot 0 inch",
+    FOOT_9_1 = "9 foot 1 inch",
+    FOOT_9_2 = "9 foot 2 inch",
+    FOOT_9_3 = "9 foot 3 inch",
+    FOOT_9_4 = "9 foot 4 inch",
+    FOOT_9_5 = "9 foot 5 inch",
+    FOOT_9_6 = "9 foot 6 inch",
+    FOOT_9_7 = "9 foot 7 inch",
+    FOOT_9_8 = "9 foot 8 inch",
+    FOOT_9_9 = "9 foot 9 inch",
+    FOOT_9_10 = "9 foot 10 inch",
+    FOOT_9_11 = "9 foot 11 inch"
+}
+
+export enum EducationLevel {
+    PRIMARY_EDUCATION = 'Primary Education (Class 1-5)',
+    JUNIOR_SECONDARY = 'Junior Secondary (Class 6-8)',
+    SSC = 'Secondary School Certificate (SSC)',
+    HSC = 'Higher Secondary Certificate (HSC)',
+    DIPLOMA = 'Diploma (Technical/Vocational)',
+    BACHELORS_DEGREE = `Bachelor's Degree`,
+    MASTERS_DEGREE = `Master's Degree`,
+    DOCTORATE = 'Doctorate (PhD)',
+    CERTIFICATE_COURSE = 'Certificate Course',
+    OTHER = 'Other'
+}
+
+export interface Education {
+    level: EducationLevel;
+    certificate: string;
+    institution: string;
+    yearOfCompletion: number;
+    grade?: string;
+    additionalInfo?: string;
+}
+
+export interface IUserPreferences {
+    education?: string[];
+    location?: string[];
+    weight?: {
+        min: number;
+        max: number;
+    };
+}
+
+export enum Religion {
+    CHRISTIANITY = "Christianity",
+    ISLAM = "Islam",
+    HINDUISM = "Hinduism",
+    BUDDHISM = "Buddhism",
+    JUDAISM = "Judaism",
+    SIKHISM = "Sikhism",
+    BAHAI_FAITH = "Bahá'í Faith",
+    JAINISM = "Jainism",
+    SHINTO = "Shinto",
+    TAOISM = "Taoism",
+    CONFUCIANISM = "Confucianism",
+    ZOROASTRIANISM = "Zoroastrianism",
+    TRADITIONAL_AFRICAN = "Traditional African Religions",
+    NATIVE_AMERICAN = "Native American Religions",
+    RASTAFARIANISM = "Rastafarianism",
+    WICCA = "Wicca",
+    PAGANISM = "Paganism"
+}
+
+export enum Language {
+    MANDARIN_CHINESE = "Mandarin Chinese",
+    SPANISH = "Spanish",
+    ENGLISH = "English",
+    HINDI = "Hindi",
+    ARABIC = "Arabic",
+    BENGALI = "Bengali",
+    PORTUGUESE = "Portuguese",
+    RUSSIAN = "Russian",
+    JAPANESE = "Japanese",
+    PUNJABI = "Punjabi",
+    GERMAN = "German",
+    JAVANESE = "Javanese",
+    WU_CHINESE = "Wu Chinese",
+    TELUGU = "Telugu",
+    VIETNAMESE = "Vietnamese",
+    MARATHI = "Marathi",
+    FRENCH = "French",
+    KOREAN = "Korean",
+    TAMIL = "Tamil",
+    ITALIAN = "Italian",
+    TURKISH = "Turkish",
+    URDU = "Urdu",
+    GUJARATI = "Gujarati",
+    POLISH = "Polish",
+    UKRAINIAN = "Ukrainian",
+    PERSIAN = "Persian",
+    MALAY = "Malay",
+    KANNADA = "Kannada",
+    XIANG_CHINESE = "Xiang Chinese",
+    MALAYALAM = "Malayalam",
+    SUNDANESE = "Sundanese",
+    HAUSA = "Hausa",
+    ODIA = "Odia",
+    BURMESE = "Burmese",
+    HAKKA_CHINESE = "Hakka Chinese",
+    TAGALOG = "Tagalog/Filipino",
+    CANTONESE = "Yue Chinese/Cantonese",
+    THAI = "Thai",
+    SWAHILI = "Swahili",
+    ROMANIAN = "Romanian",
+    DUTCH = "Dutch",
+    KURDISH = "Kurdish",
+    YORUBA = "Yoruba",
+    AMHARIC = "Amharic",
+    INDONESIAN = "Indonesian",
+    GREEK = "Greek",
+    CZECH = "Czech",
+    SINDHI = "Sindhi",
+    UZBEK = "Uzbek",
+    HUNGARIAN = "Hungarian",
+    BELARUSIAN = "Belarusian",
+    HEBREW = "Hebrew",
+    AZERBAIJANI = "Azerbaijani",
+    SLOVAK = "Slovak",
+    BULGARIAN = "Bulgarian",
+    SERBIAN = "Serbian",
+    DANISH = "Danish",
+    FINNISH = "Finnish",
+    NORWEGIAN = "Norwegian",
+    SWEDISH = "Swedish",
+    CROATIAN = "Croatian",
+    LITHUANIAN = "Lithuanian",
+    SLOVENIAN = "Slovenian",
+    LATVIAN = "Latvian",
+    ESTONIAN = "Estonian",
+    GEORGIAN = "Georgian",
+    ARMENIAN = "Armenian",
+    ALBANIAN = "Albanian",
+    MONGOLIAN = "Mongolian",
+    KAZAKH = "Kazakh",
+    NEPALI = "Nepali",
+    ASSAMESE = "Assamese",
+    TIBETAN = "Tibetan",
+    KHMER = "Khmer",
+    LAO = "Lao",
+    PASHTO = "Pashto",
+    ZULU = "Zulu",
+    XHOSA = "Xhosa",
+    IGBO = "Igbo"
+}
+
+
+
+
+interface IPhoneCountry {
+    name : string ;
+    phone_code :string;
+}
+
+export interface IPhone {
+    country: IPhoneCountry;  
+    number: string;  
+}
+
+export interface IUser extends Document {
+    profileCreatedBy: ProfileCreatedBy;
+    profileImage : IUserImage,
+    userImages : IUserImage[],
+    gender: Gender;
+    name: string;
+    dateOfBirth: Date;
+    email: string;
+    height: Height;
+    age: number;
+    weight: number;
+    isEducated: boolean;
+    education: Education[];
+    country: string;
+    address: string;
+    phoneInfo: IPhone;
+    languages: Language[];
+    religion: Religion;
+    preferences: IUserPreferences;
+    createdAt: Date;
+}
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,1 +1,158 @@
 /* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
+
+import mongoose, { Schema } from 'mongoose';
+import { IUser, ProfileCreatedBy, Gender, Height, Religion, Language, EducationLevel } from '../lib/types/user.types';
+import { countryCodes } from '../lib/data/countryCodes';
+
+const userSchema = new Schema<IUser>({
+    profileCreatedBy: {
+        type: String,
+        enum: Object.values(ProfileCreatedBy),
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true ,
+        
+    },
+    profileImage : {
+        url : {
+            type :String ,
+            required : function () { return !!this.profileImage?.id }  
+        },
+        id : {
+            type :String ,
+            required: function () { return !!this.profileImage?.url } 
+        }
+    },
+    userImages :[{
+        url : {
+            type :String ,
+            required : false
+        },
+        id : {
+            type :String ,
+            required: false
+        }
+    }],
+    gender: {
+        type: String,
+        enum: Object.values(Gender),
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    height: {
+        type: String,
+        enum: Object.values(Height),
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    isEducated: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    education: [{
+        level: {
+            type: String,
+            required: function () { return this.isEducated; },
+            enum: Object.values(EducationLevel)
+        },
+        certificate: {
+            type: String,
+            required: function () { return this.isEducated; }
+        },
+        institution: {
+            type: String,
+            required: function () { return this.isEducated; }
+        },
+        yearOfCompletion: {
+            type: Number,
+            required: function () { return this.isEducated; }
+        },
+        grade: {
+            type: String,
+            required: false
+        },
+        additionalInfo: {
+            type: String,
+            required: false
+        }
+    }],
+    country: {
+        type: String,
+        required: true,
+    },
+    address:{
+        type : String ,
+        required : true,
+        minlength : 40 ,
+        maxlength : 120 ,
+    },
+    phoneInfo: {
+        number: {
+            type: String,
+            required: true
+        },
+        country: {
+            name: {
+                type: String,
+                enum: countryCodes.map(element => element.country),
+                required: true
+            },
+            phone_code: {
+                type: String,
+                required: true,
+                enum: countryCodes.map(element => element.code),
+                maxlength: 5
+            }
+        }
+    },
+    languages: [{
+        type: String,
+        enum: Object.values(Language),
+        required: true
+    }],
+    religion: {
+        type: String,
+        enum: Object.values(Religion),
+        required: true
+    },
+    preferences: {
+        education: [{
+            type: String
+        }],
+        location: [{
+            type: String
+        }],
+        weight: {
+            min: Number,
+            max: Number
+        }
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+export const User = mongoose.model<IUser>('User', userSchema);
