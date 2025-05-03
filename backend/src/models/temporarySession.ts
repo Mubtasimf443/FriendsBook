4a01e1ced71bdeca793647921c579185c745a472
Mubtasim <mubtasimf443@gmail.com>
2025-05-03 07:24:19 +0600

    temporarySession.ts created

---
+++ backend/src/models/temporarySession.ts
@@ -0,0 +1,46 @@
+/* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
+
+import mongoose, { Document, Schema } from 'mongoose';
+
+export enum TemporarySessionNames {
+    AUTH_SESSION = "auth_session",
+    REGISTRATION_SESSION = 'registration_session'
+}
+
+interface ITemporarySession extends Document {
+    name: TemporarySessionNames;
+    value: string;
+    key?: string;
+    created_at: Date;
+    updated_at?: Date;
+}
+
+let temporarySessionSchema = new Schema<ITemporarySession>(
+    {
+        name: {
+            type: String,
+            required: true,
+            enum: Object.values(TemporarySessionNames)
+        },
+        key: {
+            type: String,
+            required: false,
+        },
+        value: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        }
+    }
+);
+
+temporarySessionSchema.index({ created_at: 1 }, { expireAfterSeconds: 600 });
+// The temporary session will automatically be deleted after 600 seconds of creation
+
+const TemporarySession = mongoose.model<ITemporarySession>('TemporarySession', temporarySessionSchema);
+export default TemporarySession;
