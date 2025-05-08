5bb5446d61c511362f7cb2b0de65af40e2911557
Mubtasim <mubtasimf443@gmail.com>
2025-05-08 21:12:51 +0600

    models srcmodelsLikedProfile.ts srcmodelsRequestMobileNumberView.ts srcmodelsSendMailedProfile.ts srcmodelsSmsSendedProfile.ts

--- backend/src/lib/schema/activity.schema.ts
+++ backend/src/lib/schema/activity.schema.ts
@@ -6,3 +6,22 @@ import { _idValidator } from "./schemaComponents";
 export const shortListSchema = z.object({
     shortListedId: _idValidator
 });   
+
+
+export const likeProfileSchema = z.object({
+    likedId: _idValidator
+});
+
+export const sendMailSchema = z.object({
+    receiverId: _idValidator,
+    message: z.string().optional()
+});
+
+export const sendSmsSchema = z.object({
+    receiverId: _idValidator,
+    message: z.string().optional()
+});
+
+export const requestMobileNumberSchema = z.object({
+    requestedId: _idValidator
+});
